f51df11ae46c7719c57281ed8b43a3056e5ff0f2
Poshan <poshan.niraula@gmail.com>
2014-08-20 16:39:54 +0545

    gui interactivity added

---
+++ js/gui-interactivity.js
@@ -0,0 +1,10 @@
+	$('#profile-container').hide();
+	// $('#viewing')
+	function hideProfileTab(){
+		$('#open-profile-container').show();
+		$('#profile-container').hide();
+	}
+	function openTheContainer(){
+		$('#profile-container').show();
+		$('#open-profile-container').hide();
+	}
